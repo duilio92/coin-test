1b35bd40a6bd5b349d6e3b198a1396f32b1c3127
Duilio <duilio.92@gmail.com>
2018-07-29 19:35:06 -0300

    problemas con los permisos no pude probar si esta bien lo del crear

--- ripiotest/transactions/permissions.py
+++ ripiotest/transactions/permissions.py
@@ -1,13 +1,21 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+from coins.models import CoinAccount
 
 
 class OwnerCreateOrReadOnly(BasePermission):
     """Owner can start a transacction,. Everyone else is rejected."""
 
-    def has_base_permission(self, request, view):
+    def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
-            return request.is_staff
-        return view.action == 'create' and request.user.is_authenticated
+            return request.user.is_staff
+        if view.action == 'create':
+            try:
+                origin = CoinAccount.objects.get(
+                    pk=request.parser_context["kwargs"]["origin"]
+                )
+                return origin.main_account.user == request.user
+            except:
+                return False
 
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
@@ -15,3 +23,4 @@ class OwnerCreateOrReadOnly(BasePermission):
                     request.user == obj.destination.user or
                     request.user == obj.origin.user
                     )
+        return False
