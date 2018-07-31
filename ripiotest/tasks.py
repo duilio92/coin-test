44671aeb78c3ae057220d80a2bd0dca73052c369
Duilio <duilio.92@gmail.com>
2018-07-30 21:20:41 -0300

    subo version del task de prueba funcionando para celery

--- ripiotest/tasks.py
+++ ripiotest/tasks.py
@@ -1,15 +1,18 @@
 from celery import Celery
 from celery_once import QueueOnce
 from time import sleep
-
-celery = Celery('tasks', broker='amqp://localhost')
-celery.conf.ONCE = {
-    'backend': 'celery_once.backends.Redis',
-    'settings': {
-        'url': 'redis://localhost:6379/0',
-        'default_timeout': 60 * 60
-    }
-}
+#from transactions.models import Transaction
+celery = Celery(
+    'tasks',
+    backend='redis://127.0.0.1:6379/0',
+    broker='pyamqp://guest@localhost//')
+# celery.conf.ONCE = {
+#     'backend': 'celery_once.backends.Redis',
+#     'settings': {
+#         'url': 'redis://127.0.0.1:6379/0',
+#         'default_timeout': 60 * 60
+#     }
+# }
 
 
 @celery.task
@@ -17,7 +20,12 @@ def add(x, y):
     return x + y
 
 
-@celery.task(base=QueueOnce)
+@celery.task
 def slow_task():
     sleep(30)
     return "Done!"
+
+
+# @celery.task(base=QueueOnce)
+# def create_transaction(transaction):
+#     Transaction.objects.create(transaction)
